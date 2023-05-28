0a029e4b0c560c7d0140ffec812977f8ece8fee5
Fatih Kabay <mfthkabay@gmail.com>
2023-05-28 16:21:51 +0300

    added files for fixed the problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
-
+    <React.StrictMode>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,13 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+export module App {
+    export class SomeClass {
+      getName(): string {
+        return 'name';
+      }
+    }
+    export class OtherClass {
+      getName(): string {
+        return 'name';
+      }
+    }
+  }
